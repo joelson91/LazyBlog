2ccc4b63efaf0f1c3cf82e5c98e271f322cfdfd0
joelson <joelsonandradep@gmail.com>
2025-03-29 10:09:20 -0300

    feat: add Post and Category models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+# Post Model
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, blank=True)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-post_date']
+
+
+# Category Model
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Categories'
